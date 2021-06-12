1b2b60395e6121bdbbb730907468ed5f1109d7b7
caio <caiolaytynherbr@gmail.com>
2021-06-11 23:33:38 -0300

    middle of refactoring stage

--- app.py
+++ app.py
@@ -1,91 +1,160 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import pandas as pd 
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import sys
 
-def main():
-    file_name = input("File name: ")
-    column_name = input("Column name: ")
-    number_of_data = int(input("Amount of data: "))
-    number_of_channels = int(input("Number of bins: "))
-    round_coeff = int(input("Round coeffitient: "))
+class App(QMainWindow):
 
-    table = pd.read_excel(file_name)
-    data = table[column_name]
-    data = [data[i] for i in range(number_of_data)]
-    
-    average = calculate_average(data)
-    lower_bound = min(data)
-    upper_bound = max(data)
-    stand_dev = calculate_stand_dev(data, average, number_of_data)
-
-    channel_size = calculate_channel_size(lower_bound, upper_bound, number_of_channels)
-    middle_values = calculate_middle_values(lower_bound, channel_size, number_of_channels)
-    frequency = count_freq(data, middle_values, number_of_channels, channel_size, round_coeff)
-    height_values = exp_prob_dens(frequency, number_of_data, channel_size)
+    def __init__(self):
+        super(App, self).__init__()
+        self.width = 500
+        self.height = 650
+        self.title = "GUI Prototype"
+        self.spacing_left = 200
+        self.spacing_top = 200
+        self.initUI()
 
 
-    if sum(frequency) != number_of_data:
-        print("Missing data")
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.spacing_left, self.spacing_top, self.width, self.height)
+
+        self.label_file = QtWidgets.QLabel(self)
+        self.label_file.setText("File name")
+        self.label_file.move(30, 30)
+
+        self.text_file = QtWidgets.QLineEdit(self)
+        self.text_file.resize(350, 22)
+        self.text_file.move(30, 55)
+
+        self.label_column = QtWidgets.QLabel(self)
+        self.label_column.setText("Column name")
+        self.label_column.move(30, 80)
+
+        self.text_column = QtWidgets.QLineEdit(self)
+        self.text_column.resize(350, 22)
+        self.text_column.move(30, 105)
+
+        self.label_data = QtWidgets.QLabel(self)
+        self.label_data.setText("Amount of data")
+        self.label_data.move(30, 130)
+
+        self.text_data = QtWidgets.QLineEdit(self)
+        self.text_data.resize(350, 22)
+        self.text_data.move(30, 155)
+
+        self.label_bins = QtWidgets.QLabel(self)
+        self.label_bins.setText("Number of bins")
+        self.label_bins.move(30, 180)
+
+        self.text_bins = QtWidgets.QLineEdit(self)
+        self.text_bins.resize(350, 22)
+        self.text_bins.move(30, 205)
+
+        self.label_round = QtWidgets.QLabel(self)
+        self.label_round.setText("Round coefficient")
+        self.label_round.move(30, 230)
+
+        self.text_round = QtWidgets.QLineEdit(self)
+        self.text_round.resize(350, 22)
+        self.text_round.move(30, 255)
+
+        self.button_histogram = QtWidgets.QPushButton(self)
+        self.button_histogram.setText("Make histogram")
+        self.button_histogram.move(30, 290)
+        self.button_histogram.clicked.connect(self.make_histogram)
 
 
-    x = np.linspace(lower_bound, upper_bound, 1000)
-    g_teo = (1 / (stand_dev * np.sqrt(2 * np.pi))) * np.exp(-0.5 * (((x - average) / stand_dev)**2))
+    def make_histogram(self):
+        file_name = input("File name: ")
+        column_name = input("Column name: ")
+        number_of_data = int(input("Amount of data: "))
+        number_of_channels = int(input("Number of bins: "))
+        round_coeff = int(input("Round coeffitient: "))
 
-    plt.bar(middle_values, height_values, channel_size)
-    
-    plt.plot(x, g_teo, 'r')
-    plt.xlabel("x")
-    plt.ylabel("Probability Density")
+        table = pd.read_excel(file_name)
+        data = table[column_name]
+        data = [data[i] for i in range(number_of_data)]
+        
+        average = calculate_average(data)
+        lower_bound = min(data)
+        upper_bound = max(data)
+        stand_dev = calculate_stand_dev(data, average, number_of_data)
 
-    plt.show()
-
-
-def calculate_average(arr):
-    return sum(arr) / len(arr)
-
-
-def calculate_stand_dev(arr, average, number):
-    big_sum = 0
-    for i in range(number):
-        big_sum += (arr[i] - average)**2 / (number - 1)
-    return np.sqrt(big_sum)
-
-
-def theo_prob_dens(x, stand_dev, average):
-    return (1 / (stand_dev * np.sqrt(2 * np.pi))) * np.exp(-0.5 * (((x - average) / stand_dev)**2))
+        channel_size = calculate_channel_size(lower_bound, upper_bound, number_of_channels)
+        middle_values = calculate_middle_values(lower_bound, channel_size, number_of_channels)
+        frequency = count_freq(data, middle_values, number_of_channels, channel_size, round_coeff)
+        height_values = exp_prob_dens(frequency, number_of_data, channel_size)
 
 
-def calculate_channel_size(min_val, max_val, chanels):
-    return (max_val - min_val) / chanels
+        if sum(frequency) != number_of_data:
+            print("Missing data")
 
 
-def calculate_middle_values(min_val, channel_size, channel_count):
-    values = []
-    for i in range(channel_count):
-        values.append(min_val + ((i + 1) - 0.5) * channel_size)
-    return values
+        x = np.linspace(lower_bound, upper_bound, 1000)
+        g_teo = (1 / (stand_dev * np.sqrt(2 * np.pi))) * np.exp(-0.5 * (((x - average) / stand_dev)**2))
+
+        plt.bar(middle_values, height_values, channel_size)
+        
+        plt.plot(x, g_teo, 'r')
+        plt.xlabel("x")
+        plt.ylabel("Probability Density")
+
+        plt.show()
 
 
-def count_freq(arr, middle, channel_count, channel_size, round_coeff):
-    values = [[]] * channel_count
-    for i in range(channel_count):
-        left_bound = np.round(middle[i] - channel_size / 2, round_coeff)
-        right_bound = np.round(middle[i] + channel_size / 2, round_coeff)
-        values[i] = [j for j in arr if j >= left_bound and j < right_bound]
-    
-    while values[0].count(min(arr)) < arr.count(min(arr)):
-        values[0].append(min(arr))
-    while values[channel_count - 1].count(max(arr)) < arr.count(max(arr)):
-        values[channel_count - 1].append(max(arr))
+    @staticmethod
+    def calculate_average(arr):
+        return sum(arr) / len(arr)
 
-    return [len(i) for i in values]
+    @staticmethod
+    def calculate_stand_dev(arr, average, number):
+        big_sum = 0
+        for i in range(number):
+            big_sum += (arr[i] - average)**2 / (number - 1)
+        return np.sqrt(big_sum)
 
+    @staticmethod
+    def theo_prob_dens(x, stand_dev, average):
+        return (1 / (stand_dev * np.sqrt(2 * np.pi))) * np.exp(-0.5 * (((x - average) / stand_dev)**2))
 
-def exp_prob_dens(arr, data_count, channel_size):
-    frequency = list(map(lambda x: x / data_count, arr))
-    prob_dens = list(map(lambda x: x / channel_size, frequency))
-    return prob_dens
+    @staticmethod
+    def calculate_channel_size(min_val, max_val, chanels):
+        return (max_val - min_val) / chanels
+
+    @staticmethod
+    def calculate_middle_values(min_val, channel_size, channel_count):
+        values = []
+        for i in range(channel_count):
+            values.append(min_val + ((i + 1) - 0.5) * channel_size)
+        return values
+
+    @staticmethod
+    def count_freq(arr, middle, channel_count, channel_size, round_coeff):
+        values = [[]] * channel_count
+        for i in range(channel_count):
+            left_bound = np.round(middle[i] - channel_size / 2, round_coeff)
+            right_bound = np.round(middle[i] + channel_size / 2, round_coeff)
+            values[i] = [j for j in arr if j >= left_bound and j < right_bound]
+        
+        while values[0].count(min(arr)) < arr.count(min(arr)):
+            values[0].append(min(arr))
+        while values[channel_count - 1].count(max(arr)) < arr.count(max(arr)):
+            values[channel_count - 1].append(max(arr))
+
+        return [len(i) for i in values]
+
+    @staticmethod
+    def exp_prob_dens(arr, data_count, channel_size):
+        frequency = list(map(lambda x: x / data_count, arr))
+        prob_dens = list(map(lambda x: x / channel_size, frequency))
+        return prob_dens
 
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    win = App()
+    win.show()
+    sys.exit(app.exec_())
