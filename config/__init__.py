135ae0add82044016291db222a1528a63346cd02
caiolaytynher <caiolaytynherbr@gmail.com>
2022-11-17 00:00:04 -0300

    changed from strings for paths and added templates

--- config/__init__.py
+++ config/__init__.py
@@ -1,2 +1,11 @@
-DATA = "./data.json"
-STYLESHEET = "./styles.css"
+from pathlib import Path
+
+DATA = Path.cwd().joinpath("./data.json")
+STYLESHEET = Path.cwd().joinpath("./styles.css")
+DATA_TEMPLATE = {
+    "file": "",
+    "column": "",
+    "amount": "",
+    "bins": "",
+    "imgname": "histogram",
+}
