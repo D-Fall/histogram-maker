3eb7b8dc701626368e8df199e502210e7aa38632
caiolaytynher <caiolaytynherbr@gmail.com>
2022-11-16 23:04:41 -0300

    added update file for data objects

--- config/handlers.py
+++ config/handlers.py
@@ -1,4 +1,7 @@
 import json
+from pathlib import Path
+
+from config.data import Data
 
 
 def load_json(filepath: str) -> dict:
@@ -26,3 +29,8 @@ def load_json(filepath: str) -> dict:
 def update_file(filepath: str, data: dict) -> None:
     with open(filepath, "w") as f:
         json.dump(data, f, indent=2)
+
+
+def update_data(filepath: Path, data: Data) -> None:
+    with open(filepath, "w") as data_file:
+        json.dump(data.__dict__, data_file, indent=2)
