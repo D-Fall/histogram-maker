a2c9a81ecf2e5d13ef6d6f544d0d4c9c44ef259a
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-07 22:15:06 -0300

    rename testing and updated function names

---
+++ tests/test_plotting.py
@@ -0,0 +1,31 @@
+from controller.plotting import create_histogram, save_histogram
+
+from pathlib import Path
+
+import pytest
+import numpy as np
+from matplotlib.figure import Figure
+
+
+@pytest.fixture
+def fig() -> Figure:
+    x = np.linspace(-1, 1, 100)
+    y = x**2
+    data = np.random.randn(100)
+
+    return create_histogram((x, y), data, 7)
+
+
+def test_create_histogram(fig: Figure) -> None:
+    assert isinstance(fig, Figure)
+
+
+def test_save_histogram(fig: Figure) -> None:
+    filepath = Path.cwd() / "tests/mock/test_hist.png"
+
+    if filepath.exists():
+        filepath.unlink()
+
+    save_histogram(fig, filepath)
+
+    assert filepath.exists()
