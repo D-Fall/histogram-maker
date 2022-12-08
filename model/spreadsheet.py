5e79dc7b09855c7ba1d01eef7f5c99144201e6b6
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-07 21:31:29 -0300

    now it only reads the dataframe

---
+++ model/spreadsheet.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from pandas import read_excel, DataFrame
+
+
+def get_data_frame(filepath: Path) -> DataFrame:
+    """
+    Read data frame.
+    """
+    assert isinstance(filepath, Path)
+    assert filepath.exists()
+
+    return read_excel(filepath)
