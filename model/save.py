663245f970ba2be8b083bf4aae4bbb7f090084ba
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-07 21:30:43 -0300

    using pickle serialization to save data

---
+++ model/save.py
@@ -0,0 +1,22 @@
+import pickle
+from pathlib import Path
+
+from .data import Data
+
+
+def save(data: Data, filepath: Path) -> None:
+    """
+    Save current data.
+    Serialize the Data object.
+    """
+    with open(filepath, "wb") as file:
+        pickle.dump(data, file)
+
+
+def load(filepath: Path) -> Data:
+    """
+    Load saved data.
+    Deserialize the Data object.
+    """
+    with open(filepath, "rb") as file:
+        return pickle.load(file)
