0764a6cd7d2adf9c95bccb838c037cf766dfe6eb
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-07 22:13:37 -0300

    separated saving from creating the figure

--- controller/plotting.py
+++ controller/plotting.py
@@ -1,29 +1,30 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 from numpy import array
 
 
-def create_hist(
+def create_histogram(
     func_axes: tuple[array, array],
     data: list[float],
     bins: int,
-    filename: str,
-) -> None:
+) -> Figure:
     """
     Create the histogram png file.
     """
-    if not filename.endswith(".png"):
-        filename += ".png"
-
-    path: Path = Path.cwd() / f"img/{filename}"
-
     plt.style.use(["science", "notebook", "grid"])
 
-    plt.hist(data, bins=bins, density=True)
-    plt.plot(*func_axes, "r", label="Normal distribution")
-    plt.xlabel("$x$")
-    plt.ylabel("Density")
-    plt.legend()
-    plt.savefig(path, dpi=200)
-    plt.close()
+    fig, ax = plt.subplots()
+
+    ax.hist(data, bins=bins, density=True)
+    ax.plot(*func_axes, "r", label="Normal distribution")
+    ax.set_xlabel("$x$")
+    ax.set_ylabel("Density")
+    ax.legend()
+
+    return fig
+
+
+def save_histogram(fig: Figure, filepath: Path) -> None:
+    fig.savefig(filepath, dpi=200)
