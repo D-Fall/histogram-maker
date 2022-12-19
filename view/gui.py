ed867eea27826684adb1d2d87d9d7da6bf1b0334
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-18 22:52:52 -0300

    updated the gui file to utilize components

--- view/gui.py
+++ view/gui.py
@@ -11,6 +11,15 @@ from matplotlib.figure import Figure
 
 from model.spreadsheet import get_data_frame
 from model.data import Data
+from .components.chart_section import init_chart_section
+from .components.form_section import (
+    init_file_name,
+    init_column_name,
+    init_number_of_values,
+    init_number_of_bins,
+    init_create_histogram_btn,
+    init_form_section,
+)
 
 
 class App:
@@ -32,10 +41,9 @@ class App:
     def init_ui(self):
         self.page.padding = ft.padding.all(20)
         self.page.bgcolor = ft.colors.PRIMARY_CONTAINER
-        # self.page.on_resize = self.on_resize
 
         pick_files_dialog = ft.FilePicker(on_result=self.on_file_pick)
-        pick_files = partial(
+        pick_file = partial(
             pick_files_dialog.pick_files,
             initial_directory=Path.cwd().as_posix(),
             allow_multiple=False,
@@ -43,169 +51,46 @@ class App:
 
         self.page.overlay.append(pick_files_dialog)
 
-        text_input_style = {
-            "border_radius": ft.border_radius.all(20),
-            "border_color": ft.colors.BLACK12,
-            "focused_border_color": ft.colors.PRIMARY,
-        }
-
-        input_section_title = ft.Column(
-            controls=[
-                ft.Text(
-                    value="Set histogram info",
-                    style=ft.TextThemeStyle.TITLE_LARGE,
-                ),
-                ft.Text(
-                    value="Fill in the required histogram info",
-                    style=ft.TextThemeStyle.BODY_SMALL,
-                ),
-            ],
-            spacing=5,
+        self.file_name: ft.TextField = init_file_name()
+        self.column_name: ft.Dropdown = init_column_name()
+        self.number_of_values: ft.TextField = init_number_of_values()
+        self.number_of_bins: ft.TextField = init_number_of_bins()
+        self.create_histogram_btn: ft.ElevatedButton = init_create_histogram_btn(
+            create_histogram_fn=self.create_histogram,
         )
 
-        self.file_name = ft.TextField(
-            label="Choose a spreadsheet file",
-            read_only=True,
-            expand=True,
-            **text_input_style,
-        )
-        pick_file = ft.Row(
-            controls=[
-                self.file_name,
-                ft.IconButton(
-                    icon=ft.icons.FILE_OPEN,
-                    on_click=pick_files,
-                    tooltip="Pick a file",
-                    style=ft.ButtonStyle(
-                        color=ft.colors.ON_PRIMARY,
-                        bgcolor=ft.colors.PRIMARY,
-                    ),
-                ),
-            ],
-        )
-        self.column_name = ft.Dropdown(
-            label="Select a column",
-            # prefix_icon=ft.icons.VIEW_COLUMN,
-            **text_input_style,
-        )
-        self.amount = ft.TextField(
-            label="Number of values to use",
-            **text_input_style,
-        )
-        self.bins = ft.TextField(
-            label="Number of bins",
-            **text_input_style,
-        )
-        self.create_histogram_btn = ft.Container(
-            content=ft.ElevatedButton(
-                text="Create histogram",
-                icon=ft.icons.BAR_CHART,
-                style=ft.ButtonStyle(
-                    color=ft.colors.ON_PRIMARY,
-                    # color={
-                    #     ft.MaterialState.HOVERED: ft.colors.WHITE,
-                    #     # ft.MaterialState.FOCUSED: ft.colors.,
-                    #     ft.MaterialState.DEFAULT: ft.colors.ON_PRIMARY,
-                    # },
-                    bgcolor=ft.colors.PRIMARY,
-                    padding=ft.padding.all(20),
-                ),
-                on_click=self.create_histogram,
-            ),
-            # margin=ft.margin.only(top=50),
-        )
-
-        self.text_based_inputs = [
+        self.form_inputs = [
+            self.file_name,
             self.column_name,
-            self.amount,
-            self.bins,
+            self.number_of_values,
+            self.number_of_bins,
         ]
 
-        input_section_content = ft.Column(
-            controls=[
-                input_section_title,
-                ft.Divider(
-                    color=ft.colors.BACKGROUND,
-                    height=30,
-                ),
-                pick_file,
-                self.column_name,
-                self.amount,
-                self.bins,
-                ft.Divider(
-                    color=ft.colors.BLACK12,
-                    height=30,
-                ),
-                self.create_histogram_btn,
-            ],
-            spacing=15,
-            expand=True,
+        form_section = init_form_section(
+            file_name=self.file_name,
+            pick_file=pick_file,
+            column_name=self.column_name,
+            number_of_values=self.number_of_values,
+            number_of_bins=self.number_of_bins,
+            create_histogram_btn=self.create_histogram_btn,
         )
 
-        chart_section_title = ft.Column(
-            controls=[
-                ft.Text(
-                    value="Histogram chart",
-                    style=ft.TextThemeStyle.TITLE_LARGE,
-                ),
-                ft.Text(
-                    value="Visualization of the histogram chart",
-                    style=ft.TextThemeStyle.BODY_SMALL,
-                ),
-            ],
-            spacing=5,
-        )
         self.chart = MatplotlibChart()
-        chart_section = ft.Column(
+
+        chart_section = init_chart_section(chart=self.chart)
+
+        main_content = ft.Row(
             controls=[
-                chart_section_title,
-                ft.Divider(
-                    color=ft.colors.BACKGROUND,
-                    height=30,
-                ),
-                self.chart,
-            ]
-        )
-
-        input_section = ft.Container(
-            content=input_section_content,
-            bgcolor=ft.colors.BACKGROUND,
-            padding=ft.padding.all(30),
-            border_radius=ft.border_radius.all(20),
-            expand=True,
-        )
-
-        histogram_chart = ft.Container(
-            content=chart_section,
-            expand=True,
-            bgcolor=ft.colors.BACKGROUND,
-            border_radius=ft.border_radius.all(20),
-            padding=ft.padding.all(30),
-        )
-
-        self.main_content = ft.Row(
-            controls=[
-                input_section,
-                histogram_chart,
+                form_section,
+                chart_section,
             ],
             spacing=20,
         )
 
-        self.page.add(self.main_content)
-
-    def on_resize(self, e: ControlEvent) -> None:
-        inputs_height: float = (
-            e.control.height - e.control.padding.bottom - e.control.padding.top
-        )
-        for control in self.main_content.controls:
-            control.height = inputs_height
-
-        print(f"{self.main_content.height = !r}")
-        print(f"{e.control.height = !r}")
-
-        self.page.update()
+        self.page.add(main_content)
 
     def load_data(self) -> None:
+        """ """
         if self.data is None:
             self.file_name.value = "No file selected."
             self.column_name.visible = False
@@ -229,8 +114,8 @@ class App:
             self.column_name.options = options
             self.column_name.value = self.data.column
 
-            self.amount.value = self.data.amount or ""
-            self.bins.value = self.data.bins or ""
+            self.number_of_values.value = self.data.amount or ""
+            self.number_of_bins.value = self.data.bins or ""
 
             self.page.update()
 
@@ -245,13 +130,13 @@ class App:
             self.page.update()
 
     def update_data(self) -> None:
-        for input_dialog in self.text_based_inputs:
+        for input_dialog in self.form_inputs:
             if not input_dialog.value:
                 return None
 
         self.data.column = self.column_name.value
-        self.data.amount = int(self.amount.value)
-        self.data.bins = int(self.bins.value)
+        self.data.amount = int(self.number_of_values.value)
+        self.data.bins = int(self.number_of_bins.value)
 
     def create_histogram(self, e: ControlEvent) -> None:
         self.update_data()
