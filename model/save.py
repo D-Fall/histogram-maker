bafcadc8efc58fc966caeccdaa7254a5b0e7a6cb
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-19 02:18:15 -0300

    now save returns a default object if no file

--- model/save.py
+++ model/save.py
@@ -13,13 +13,13 @@ def save(data: Data, filepath: Path) -> None:
         pickle.dump(data, file)
 
 
-def load(filepath: Path) -> Data | None:
+def load(filepath: Path) -> Data:
     """
     Load saved data.
     Deserialize the Data object.
     """
     if not filepath.exists():
-        return None
+        return Data()
 
     with open(filepath, "rb") as file:
         return pickle.load(file)
