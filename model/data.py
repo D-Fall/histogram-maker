e17ced32c9a4224e4c9cf189652112005577d653
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-19 02:19:06 -0300

    updated data field names

--- model/data.py
+++ model/data.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Data:
-    spreadsheet_file: Path
-    column: str
-    amount: int
-    bins: int
+    spreadsheet_file: Path = Path()
+    column_name: str = ""
+    number_of_values: int = 0
+    number_of_bins: int = 0
