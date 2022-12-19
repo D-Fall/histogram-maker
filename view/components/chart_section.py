6d952436a66e3b18316686c556a80552ef94d6e6
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-18 22:52:27 -0300

    created a chart section component

---
+++ view/components/chart_section.py
@@ -0,0 +1,37 @@
+import flet as ft
+from flet.matplotlib_chart import MatplotlibChart
+
+
+def init_chart_section(chart: MatplotlibChart) -> ft.Container:
+    title = ft.Column(
+        controls=[
+            ft.Text(
+                value="Histogram chart",
+                style=ft.TextThemeStyle.TITLE_LARGE,
+            ),
+            ft.Text(
+                value="Visualization of the histogram chart",
+                style=ft.TextThemeStyle.BODY_SMALL,
+            ),
+        ],
+        spacing=5,
+    )
+
+    chart_content = ft.Column(
+        controls=[
+            title,
+            ft.Divider(
+                color=ft.colors.BACKGROUND,
+                height=30,
+            ),
+            chart,
+        ]
+    )
+
+    return ft.Container(
+        content=chart_content,
+        expand=True,
+        bgcolor=ft.colors.BACKGROUND,
+        border_radius=ft.border_radius.all(20),
+        padding=ft.padding.all(30),
+    )
