0209a763aff1f629f5b214aa6f23a3a7912b6231
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-21 01:37:23 -0300

    now save logic is part of the data class

--- model/data.py
+++ model/data.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+import pickle
 from pathlib import Path
 from dataclasses import dataclass
 
@@ -8,3 +11,23 @@ class Data:
     column_name: str = ""
     number_of_values: int = 0
     number_of_bins: int = 0
+
+    def save(self, filepath: Path):
+        """
+        Save current data.
+        Serialize the Data object.
+        """
+        with open(filepath, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def load(cls, filepath: Path) -> Data:
+        """
+        Load saved data.
+        Deserialize the Data object.
+        """
+        if not filepath.exists():
+            return cls()
+
+        with open(filepath, "rb") as file:
+            return pickle.load(file)
--- tests/test_save.py
+++ tests/test_save.py
@@ -1,13 +1,42 @@
 from pathlib import Path
 
-from model.save import load
+import pytest
+
+from model.data import Data
 
 
-def test_fail_to_load() -> None:
+@pytest.fixture
+def filepath() -> Path():
+    return Path.cwd() / "tests/mock/test_data.pickle"
+
+
+@pytest.fixture
+def data() -> Data:
+    return Data(
+        spreadsheet_file=Path.cwd(),
+        column_name="mock column name",
+        number_of_values=3,
+        number_of_bins=3,
+    )
+
+
+def test_save(filepath: Path, data: Data):
+    if filepath.exists():
+        filepath.unlink()
+
+    data.save(filepath)
+
+    assert filepath.exists()
+
+
+def test_load(filepath: Path, data: Data):
+    loaded_data = Data.load(filepath)
+
+    assert loaded_data == data
+
+
+def test_fail_to_load():
     mock_file = Path("mock_file.picke")
 
-    data = load(mock_file)
-    assert data is None
-
-    data = load(mock_file) or "mock_value"
-    assert data == "mock_value"
+    mock_data = Data.load(mock_file)
+    assert mock_data == Data()
