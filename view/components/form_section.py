f41cefd104e28222cb812a9fb1550c43748cb6c5
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-20 23:51:02 -0300

    now form section is a user control

--- view/components/form_section.py
+++ view/components/form_section.py
@@ -1,123 +1,131 @@
-from typing import Callable
-from typing import Any
+from functools import partial
+from pathlib import Path
 
 import flet as ft
 
-TEXT_INPUT_STYLE = {
-    "border_radius": ft.border_radius.all(20),
-    "border_color": ft.colors.BLACK12,
-    "focused_border_color": ft.colors.PRIMARY,
-}
 
+class FormSection(ft.UserControl):
+    """
+    Section that contains all the user inputs controls necessary to produce the
+    histogram.
 
-def init_file_name() -> ft.TextField:
-    return ft.TextField(
-        label="Choose a spreadsheet file",
-        read_only=True,
-        expand=True,
-        **TEXT_INPUT_STYLE,
-    )
+    There are some methods that have to be defined after the creation of the
+    FormSection object because they require data acess. They are:
+        - create_histogram_btn.on_click
+        - pick_files_dialog.on_result
+    """
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-def init_column_name() -> ft.Dropdown:
-    return ft.Dropdown(
-        label="Select a column",
-        **TEXT_INPUT_STYLE,
-    )
+        common_input_style = {
+            "border_radius": ft.border_radius.all(20),
+            "border_color": ft.colors.BLACK12,
+            "focused_border_color": ft.colors.PRIMARY,
+        }
 
+        self.file_name = ft.TextField(
+            label="Choose a spreadsheet file",
+            read_only=True,
+            expand=True,
+            **common_input_style,
+        )
 
-def init_number_of_values() -> ft.TextField:
-    return ft.TextField(
-        label="Number of values to use",
-        **TEXT_INPUT_STYLE,
-    )
+        self.column_name = ft.Dropdown(
+            label="Select a column",
+            **common_input_style,
+        )
 
+        self.number_of_values = ft.TextField(
+            label="Number of values to use",
+            **common_input_style,
+        )
 
-def init_number_of_bins() -> ft.TextField:
-    return ft.TextField(
-        label="Number of bins",
-        **TEXT_INPUT_STYLE,
-    )
+        self.number_of_bins = ft.TextField(
+            label="Number of bins",
+            **common_input_style,
+        )
 
-
-def init_create_histogram_btn(
-    create_histogram_fn: Callable[[ft.ControlEvent], None],
-) -> ft.Container:
-    return ft.ElevatedButton(
-        text="Create histogram",
-        icon=ft.icons.BAR_CHART,
-        style=ft.ButtonStyle(
-            color=ft.colors.ON_PRIMARY,
-            bgcolor=ft.colors.PRIMARY,
-            padding=ft.padding.all(20),
-        ),
-        on_click=create_histogram_fn,
-    )
-
-
-def init_form_section(
-    file_name: ft.TextField,
-    pick_file: Callable[[Any], None],
-    column_name: ft.Dropdown,
-    number_of_values: ft.TextField,
-    number_of_bins: ft.TextField,
-    create_histogram_btn: ft.ElevatedButton,
-) -> ft.Container:
-    title = ft.Column(
-        controls=[
-            ft.Text(
-                value="Set histogram info",
-                style=ft.TextThemeStyle.TITLE_LARGE,
+        self.create_histogram_btn = ft.ElevatedButton(
+            text="Create histogram",
+            icon=ft.icons.BAR_CHART,
+            style=ft.ButtonStyle(
+                color=ft.colors.ON_PRIMARY,
+                bgcolor=ft.colors.PRIMARY,
+                padding=ft.padding.all(20),
             ),
-            ft.Text(
-                value="Fill in the required histogram info",
-                style=ft.TextThemeStyle.BODY_SMALL,
-            ),
-        ],
-        spacing=5,
-    )
+        )
 
-    file_picker = ft.Row(
-        controls=[
-            file_name,
-            ft.IconButton(
-                icon=ft.icons.FILE_OPEN,
-                on_click=pick_file,
-                tooltip="Pick a file",
-                style=ft.ButtonStyle(
-                    color=ft.colors.ON_PRIMARY,
-                    bgcolor=ft.colors.PRIMARY,
+        self.pick_files_dialog = ft.FilePicker()
+
+        self.input_fields: dict[str, ft.TextField | ft.Dropdown] = {
+            "file_name": self.file_name,
+            "column_name": self.column_name,
+            "number_of_values": self.number_of_values,
+            "number_of_bins": self.number_of_bins,
+        }
+
+    def build(self) -> ft.Container:
+        title = ft.Column(
+            controls=[
+                ft.Text(
+                    value="Set histogram info",
+                    style=ft.TextThemeStyle.TITLE_LARGE,
                 ),
-            ),
-        ],
-    )
+                ft.Text(
+                    value="Fill in the required histogram info",
+                    style=ft.TextThemeStyle.BODY_SMALL,
+                ),
+            ],
+            spacing=5,
+        )
 
-    form_content = ft.Column(
-        controls=[
-            title,
-            ft.Divider(
-                color=ft.colors.BACKGROUND,
-                height=30,
-            ),
-            file_picker,
-            column_name,
-            number_of_values,
-            number_of_bins,
-            ft.Divider(
-                color=ft.colors.BLACK12,
-                height=30,
-            ),
-            create_histogram_btn,
-        ],
-        spacing=15,
-        expand=True,
-    )
+        pick_file = partial(
+            self.pick_files_dialog.pick_files,
+            initial_directory=Path.cwd().as_posix(),
+            allow_multiple=False,
+        )
 
-    return ft.Container(
-        content=form_content,
-        bgcolor=ft.colors.BACKGROUND,
-        padding=ft.padding.all(30),
-        border_radius=ft.border_radius.all(20),
-        expand=True,
-        col={"md": 12, "lg": 6},
-    )
+        file_picker = ft.Row(
+            controls=[
+                self.file_name,
+                ft.IconButton(
+                    icon=ft.icons.FILE_OPEN,
+                    on_click=pick_file,
+                    tooltip="Pick a file",
+                    style=ft.ButtonStyle(
+                        color=ft.colors.ON_PRIMARY,
+                        bgcolor=ft.colors.PRIMARY,
+                    ),
+                ),
+            ],
+        )
+
+        form_content = ft.Column(
+            controls=[
+                title,
+                ft.Divider(
+                    color=ft.colors.BACKGROUND,
+                    height=30,
+                ),
+                file_picker,
+                self.column_name,
+                self.number_of_values,
+                self.number_of_bins,
+                ft.Divider(
+                    color=ft.colors.BLACK12,
+                    height=30,
+                ),
+                self.create_histogram_btn,
+            ],
+            spacing=15,
+            expand=True,
+        )
+
+        return ft.Container(
+            content=form_content,
+            bgcolor=ft.colors.BACKGROUND,
+            padding=ft.padding.all(30),
+            border_radius=ft.border_radius.all(20),
+            expand=True,
+        )
