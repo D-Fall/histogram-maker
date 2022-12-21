1f3319fe845e0327ebef9dd0d09a30f6ad5958b4
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-20 23:55:38 -0300

    now chart section is a user control

--- view/components/chart_section.py
+++ view/components/chart_section.py
@@ -2,37 +2,42 @@ import flet as ft
 from flet.matplotlib_chart import MatplotlibChart
 
 
-def init_chart_section(chart: MatplotlibChart) -> ft.Container:
-    title = ft.Column(
-        controls=[
-            ft.Text(
-                value="Histogram chart",
-                style=ft.TextThemeStyle.TITLE_LARGE,
-            ),
-            ft.Text(
-                value="Visualization of the histogram chart",
-                style=ft.TextThemeStyle.BODY_SMALL,
-            ),
-        ],
-        spacing=5,
-    )
+class ChartSection(ft.UserControl):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    chart_content = ft.Column(
-        controls=[
-            title,
-            ft.Divider(
-                color=ft.colors.BACKGROUND,
-                height=30,
-            ),
-            chart,
-        ]
-    )
+        self.chart = MatplotlibChart()
 
-    return ft.Container(
-        content=chart_content,
-        expand=True,
-        bgcolor=ft.colors.BACKGROUND,
-        border_radius=ft.border_radius.all(20),
-        padding=ft.padding.all(30),
-        col={"md": 12, "lg": 6},
-    )
+    def build(self) -> ft.Container:
+        title = ft.Column(
+            controls=[
+                ft.Text(
+                    value="Histogram chart",
+                    style=ft.TextThemeStyle.TITLE_LARGE,
+                ),
+                ft.Text(
+                    value="Visualization of the histogram chart",
+                    style=ft.TextThemeStyle.BODY_SMALL,
+                ),
+            ],
+            spacing=5,
+        )
+
+        chart_content = ft.Column(
+            controls=[
+                title,
+                ft.Divider(
+                    color=ft.colors.BACKGROUND,
+                    height=30,
+                ),
+                self.chart,
+            ]
+        )
+
+        return ft.Container(
+            content=chart_content,
+            expand=True,
+            bgcolor=ft.colors.BACKGROUND,
+            border_radius=ft.border_radius.all(20),
+            padding=ft.padding.all(30),
+        )
