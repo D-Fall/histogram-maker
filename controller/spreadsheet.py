84af42b9b521c48806ac5871f60fa4fdbc6be803
caiolaytynher <caiolaytynherbr@gmail.com>
2022-12-26 20:23:20 -0300

    pep8 compliant docstring

--- controller/spreadsheet.py
+++ controller/spreadsheet.py
@@ -4,9 +4,7 @@ from pandas import read_excel, DataFrame
 
 
 def get_data_frame(filepath: Path) -> DataFrame:
-    """
-    Read data frame.
-    """
+    """Read data frame."""
     assert isinstance(filepath, Path)
     assert filepath.exists()
 
